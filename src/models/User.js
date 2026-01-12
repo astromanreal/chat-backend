27d87e2b2135586fcd985f94f8d1d83bab64450b
astromanreal <astroman@gmail.com>
2026-01-12 20:59:15 +0530

    User login issue handling

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'; // Import bcrypt
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -6,7 +7,7 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide a username'],
     unique: true,
     trim: true,
-    lowercase: true, // Enforce lowercase usernames
+    lowercase: true, 
   },
   name: {
     type: String,
@@ -30,7 +31,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: [6, 'Password must be at least 6 characters'],
-    select: false, // Do not return password by default
+    select: false, 
   },
   isVerified: {
     type: Boolean,
@@ -44,6 +45,19 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     select: false,
   },
+});
+
+// --- Add the pre-save hook here ---
+UserSchema.pre('save', async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  // Hash the password with a salt round of 10
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 export default mongoose.model('User', UserSchema);
