a42b44ab88d69c10587a4e221f72c03a80c5b97b
astromanreal <astroman@gmail.com>
2026-01-12 23:23:30 +0530

    Session expired error handling again

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,27 @@
 import jwt from 'jsonwebtoken';
 
 export default function (req, res, next) {
-    const token = req.header('x-auth-token');
+    let token;
 
+    // Check for the standard Authorization header first
+    if (req.header('Authorization') && req.header('Authorization').startsWith('Bearer ')) {
+        // Extract the token from the "Bearer <token>" string
+        token = req.header('Authorization').split(' ')[1];
+    } 
+    // Fallback to check for the old 'x-auth-token' header if you need to support both
+    else if (req.header('x-auth-token')) {
+        token = req.header('x-auth-token');
+    }
+
+    // If no token is found after checking both headers, deny authorization
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
 
     try {
+        // Verify the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // Attach the user payload to the request object
         req.user = decoded.user;
         next();
     } catch (err) {
